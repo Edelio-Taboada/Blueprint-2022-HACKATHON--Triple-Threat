5caf20afc50597994cf004c5a68aee1de6a7ad86
Diego Gonzalez <51039803+DiegoJGonzalez23@users.noreply.github.com>
2022-02-20 13:36:59 -0500

    Added Picture

--- buttons.js
+++ buttons.js
@@ -1,14 +1,24 @@
 let x = 1;
 function help(){
-    
+    let image = document.getElementById('social');
   
     if(x% 2 === 1){
-        document.getElementById("doThis").innerHTML = 'The creators of this minesweeper game are Diego Gonzalez and Edelio Taboada. They both attend Doral Academy Charter High and plan on studying computer science in the near future.';
+        document.getElementById("doThis").innerHTML = 'The creators of this minesweeper game are Diego Gonzalez and Edelio Taboada. They both attend Doral Academy Charter High and plan on studying computer science in the near future. They have both participated in competitive programming but only Edelio has dived into Web Development. This is a way to show off our new skills that we learned and display our love for Minesweeper. Here is a picture of the Two!';
         x++;
+     
+        image.src = "Assets/pic1.jpeg"
+      
+        
     }
     else{
         document.getElementById("doThis").innerHTML = '';
         x++;
+
+       
+        image.src = "Assets/large.jpg"
+        
+         
+        
     }
     
     
